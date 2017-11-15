83a98aa87f15865bdab2f6f0dab0d87d2e948859
Or Sher <or.sher@personali.com>
2017-11-15 13:59:04 +0200

    Fixed integration test

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -4,21 +4,6 @@ set -e
 
 trap "docker-compose down" EXIT
 
-if uname | grep -q Linux; then
-    export DOCKER_BIND_IP=$(ip addr | grep 'eth0:' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-elif uname | grep -q Darwin; then
-    export DOCKER_BIND_IP=$(ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}')
-else
-    echo "Unsupported operating system."
-    exit 1
-fi
-
-echo -e "\nDocker bind IP address: $DOCKER_BIND_IP\n"
-
-export KAFKA_HOST=$DOCKER_BIND_IP
-export KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://${KAFKA_HOST}:9092"
-export KAFKA_BROKERS="${KAFKA_HOST}:9092"
-
 # Building connector JAR and starting test cluster
 mvn clean package
 docker-compose up -d
@@ -41,5 +26,3 @@ http://localhost:8083/connectors
 
 # Running integration tests
 mvn verify
-
-sleep 1000
